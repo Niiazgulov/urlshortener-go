00cccfb5dcc7af98c008e81cb57b279038024ec7
Albert Niiazgulov <alertufa@gmail.com>
2023-01-07 23:15:05 +0500

    Test inc3 ver 8

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -10,26 +10,35 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var testkeymap = map[string]string{}
+
 func testRequest(t *testing.T, ts *httptest.Server, method, path string) (int, string) {
 	req, err := http.NewRequest(method, ts.URL+path, nil)
 	require.NoError(t, err)
 	resp, err := http.DefaultClient.Do(req)
 	require.NoError(t, err)
-	respBody, err := io.ReadAll(resp.Body)
-	require.NoError(t, err)
-	defer resp.Body.Close()
-	return resp.StatusCode, string(respBody)
+	if method == "POST" {
+		respBody, err := io.ReadAll(resp.Body)
+		require.NoError(t, err)
+		defer resp.Body.Close()
+		return resp.StatusCode, string(respBody)
+	} else {
+		respBody := resp.Header.Get("Location")
+		require.NoError(t, err)
+		defer resp.Body.Close()
+		return resp.StatusCode, string(respBody)
+	}
 }
 
 func TestRouter(t *testing.T) {
 	r := NewRouter()
-
 	ts := httptest.NewServer(r)
 	defer ts.Close()
-
-	statusCode, body := testRequest(t, ts, "POST", "yamba.com")
+	statusCode, body := testRequest(t, ts, "POST", "/")
+	testkeymap[body] = ts.URL
 	assert.Equal(t, http.StatusCreated, statusCode)
 	statusCode, body = testRequest(t, ts, "GET", "/{id}")
+	original := testkeymap[body]
 	assert.Equal(t, http.StatusTemporaryRedirect, statusCode)
-	assert.Equal(t, "yamba.com", body)
+	assert.Equal(t, original, body)
 }
