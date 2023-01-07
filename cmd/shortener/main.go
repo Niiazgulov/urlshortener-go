89879adaf3d25a4f6152cf1eeab0faa460939ac6
Albert Niiazgulov <alertufa@gmail.com>
2023-01-07 20:40:41 +0500

    Test inc3 ver 6

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -29,43 +29,6 @@ func Encoder(number uint64) string {
 	return encodedBuilder.String()
 }
 
-// func RemoveChar(word string) string {
-// 	return word[1:]
-// }
-
-// func NewRouter() chi.Router {
-// 	r := chi.NewRouter()
-// 	r.Use(middleware.RequestID)
-// 	r.Use(middleware.RealIP)
-// 	r.Use(middleware.Logger)
-// 	r.Use(middleware.Recoverer)
-// 	r.Route("/", func(r chi.Router) {
-// 		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-// 			rand.Seed(time.Now().UnixNano())
-// 			randint := rand.Uint64()
-// 			short := Encoder(randint)
-// 			shorturl := "http://localhost:8080/" + short
-// 			longURLByte, err := io.ReadAll(r.Body)
-// 			if err != nil {
-// 				log.Fatal(err)
-// 			}
-// 			longURL := strings.ReplaceAll(string(longURLByte), "url=", "")
-// 			longURL, _ = url.QueryUnescape(longURL)
-// 			keymap[short] = longURL
-// 			w.WriteHeader(http.StatusCreated)
-// 			w.Write([]byte(shorturl))
-// 		})
-// 		r.Get("/{id}", func(w http.ResponseWriter, r *http.Request) {
-// 			shortnew := chi.URLParam(r, "id")
-// 			originalURL := keymap[shortnew]
-// 			w.Header().Set("Location", originalURL)
-// 			w.WriteHeader(http.StatusTemporaryRedirect)
-// 		})
-// 	})
-
-// 	return r
-// }
-
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
