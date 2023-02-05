05c53c4ec39f918f46380b4975647a35561365d4
Albert Niiazgulov <alertufa@gmail.com>
2023-02-05 17:42:15 +0500

    Test 12 Iter 11

--- cmd/shortener/service/repository/databasestore.go
+++ cmd/shortener/service/repository/databasestore.go
@@ -28,14 +28,18 @@ func NewDataBaseStorqage(databasePath string) (*DataBaseStorage, error) {
 }
 
 func (d DataBaseStorage) AddURL(ctx context.Context, u URL, userID string) error {
-	addURLcommand := "INSERT INTO urls (original_url, id, user_id) VALUES ($1, $2, $3)"
-	d.DataBase.QueryRowContext(ctx, addURLcommand, u.OriginalURL, u.ShortURL, userID)
+	query := "INSERT INTO urls (original_url, id, user_id) VALUES ($1, $2, $3)"
+	_, err := d.DataBase.Exec(query, u.OriginalURL, u.ShortURL, userID)
+	if err != nil {
+		return fmt.Errorf("unable to AddURL to DB: %w", err)
+	}
+	// d.DataBase.QueryRowContext(ctx, addURLcommand, u.OriginalURL, u.ShortURL, userID)
 	return nil
 }
 
 func (d DataBaseStorage) GetURL(ctx context.Context, id string) (string, error) {
-	getURLcommand := "SELECT original_url FROM urls WHERE id = $1"
-	row := d.DataBase.QueryRowContext(ctx, getURLcommand, id)
+	query := "SELECT original_url FROM urls WHERE id = $1"
+	row := d.DataBase.QueryRowContext(ctx, query, id)
 	var originalURL string
 	err := row.Scan(&originalURL)
 	if err != nil {
@@ -45,8 +49,8 @@ func (d DataBaseStorage) GetURL(ctx context.Context, id string) (string, error) 
 }
 
 func (d DataBaseStorage) FindAllUserUrls(ctx context.Context, userID string) (map[string]string, error) {
-	selectUrlscommand := "SELECT original_url, id FROM urls WHERE user_id = $1"
-	rows, err := d.DataBase.QueryContext(ctx, selectUrlscommand, userID)
+	query := "SELECT original_url, id FROM urls WHERE user_id = $1"
+	rows, err := d.DataBase.QueryContext(ctx, query, userID)
 	if err != nil {
 		return nil, fmt.Errorf("unable to return urls from DB (FindAllUserUrls): %w", err)
 	}
